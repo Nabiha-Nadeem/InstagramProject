058b35d2cc95b3750fc8365dbf636e4366856925
Nabiha <nabihan110@Gmail.com>
2022-08-17 21:58:24 +0500

    Routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
 
-  post 'follow/user/:id' => 'follows#follow_user', as: :follow_user
-  delete 'unfollow/user' => 'follows#unfollow_user', as: :unfollow_user
-  post 'request/user' => 'requests#create', as: :request_user
-  post 'remove-request/user' => 'requests#remove_follow_request', as: :remove_request
   post 'search/user' => 'users#search', as: :search_user
 
-  resources :requests, only: %i[create index update]
-
+  resources :requests, only: %i[create index update destroy]
+  resources :follows, only: %i[create destroy]
   resources :users, only: %i[show index]
 
   resources :posts, only: %i[show create update destroy edit] do
